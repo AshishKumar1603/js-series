fe6ca35dfaf7221522df25c97c34aa70babc1b8a
Ashish Kumar <akgautam1603@gmail.com>
2025-05-17 07:36:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+ const accountId = 142234
+ let accountEmail = "akgautan1603@gmail.com"
+ var accountPassword ="665544"
+ accountcity = "gorakhpur"
+
+//  accountId = 765546 // not allowed
+
+ accountEmail = "ak@gmail"
+ Accountpassword = "223344"
+ accountcity ="goa"
+
+ console.table([accountId, accountEmail, accountPassword, accountcity])
