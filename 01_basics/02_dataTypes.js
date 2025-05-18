7477eb34f4f2ac16fcc8912c8db8ae9a6f4f0352
Ashish Kumar <akgautam1603@gmail.com>
2025-05-18 07:45:41 +0000

    COnversion of datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,32 @@
 "use strict"; //treat all js code as newer version
+
+//alert(3+3)// alert("notify")//pop up in browser //does not work herer bas we are using nodejs not browser
+
+console.log(3+3); console.log("Ashish")
+//code readability is very important
+// console.log(3+3)
+// console>log("Ashish")
+
+
+// DATA TYPES => javascript has 8 datatypes
+
+let color="blue"; // String
+let age = 23; // Number => 2 to powers 53
+let x = true;// boolean
+let y; // undefined
+let name = null
+
+console.log(typeof "blue")
+
+console.log(typeof age)
+console.log(typeof x) 
+
+console.log(typeof null)
+
+// undefined------
+//A variable that has not been assigned a value is a type of undefined.
+
+//Null----
+// it itself a 'stand alone value' means itself it is a value(just like a normal values)
+// else.g. let age =18 // 18 is a value of type 'number'
+//         similarly , let noValue = null // here, null is a value type 'object'(typeof null)=> object  
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let  score = null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log( typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN (not a number)
+//true => 1; false=> 0
+
+let Create = 1
+
+let booleanCreate = Boolean(Create)
+
+console.log(booleanCreate)
+
+// 1=> true; 0=> false
+// "" => false
+// "Ashish" => true
+
+let someNumber = 33
+
+let str_num = String(someNumber)
+console.log(str_num)
+
+console.log(typeof str_num)
