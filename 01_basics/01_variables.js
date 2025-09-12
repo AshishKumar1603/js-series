2c5dd2a1c0bca7a21ea39bccf0a709a0ed40107d
Ashish Kumar <akgautam1603@gmail.com>
2025-09-12 06:51:39 +0000

    assigning_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,3 +10,17 @@
  accountcity ="goa"
 
  console.table([accountId, accountEmail, accountPassword, accountcity])
+
+ // let gameName is a variable so we can change it's value ...
+ let gameName ="Spiderman"
+
+ gameName ="batman"
+
+ console.log(gameName);
+ 
+ //Here const is fixed one we assign it doesn't change so here it shows error..
+ const username ="Ashishdotcom"
+ username ="Ashish"
+
+ console.log("username");
+ 
