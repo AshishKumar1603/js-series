600442f1f02cd2c1afff31419f85418a1c41d09b
Ashish Kumar <akgautam1603@gmail.com>
2025-09-12 05:12:01 +0000

    printing_the_data

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -3,6 +3,8 @@ console.log("chai")
 process.stdout.write("chai");
 process.stdout.write("chai");
 
+console.table({city:"Gorakhpur"})
+
 
 // Here we Use "console.log" for print the output in new line but ther is another way 
 // print output i.e "process.stdout.write" but we didn't use this because it print the 
