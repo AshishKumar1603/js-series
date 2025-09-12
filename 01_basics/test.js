f3f56f4a6da9275e168736f68c343595aea58004
Ashish Kumar <akgautam1603@gmail.com>
2025-09-12 05:06:58 +0000

    print output

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
 console.log("Ashish")
+console.log("chai")
+process.stdout.write("chai");
+process.stdout.write("chai");
+
+
+// Here we Use "console.log" for print the output in new line but ther is another way 
+// print output i.e "process.stdout.write" but we didn't use this because it print the 
+// multiple out put in the same line
