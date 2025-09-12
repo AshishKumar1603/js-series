f4a6f90b63fae1df2422db87698437a28dd51eaa
Ashish Kumar <akgautam1603@gmail.com>
2025-09-12 11:52:14 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,7 +8,7 @@ let  score = null
 let valueInNumber = Number(score)
 
 //console.log( typeof valueInNumber)
-//console.log(valueInNumber);
+console.log(valueInNumber);
 
 //"33"=> 33
 //"33abc"=> NaN (not a number)
--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,22 +5,24 @@
 // console.log(2!=1);
 // console.log(2==1);
 
-console.log("2">1)// js converted string to number ( value conversion)
-console.log("02">1)
+// console.log("2">1)// js converted string to number ( value conversion)
+// console.log("02">1)
 
 // //***** Avoid this type of comparision******
 
-console.log(null>0); //false -- value conversion problem 
-console.log(null==0);// false  
-console.log(null>=0); // truse because comparision coverted null to be a number, treating is as 0.
+// console.log(null>0); //false -- value conversion problem 
+// console.log(null==0);// false  
+// console.log(null>=0); // true because comparision coverted null to be a number, treating is as 0.
 
 
 console.log(undefined == 0);
 console.log(undefined < 0);
 console.log(undefined == 0);
-console.log(undefined==null);
+console.log(undefined==null); //ye special case hai dono hamesha equal hote hai(loose equality==)
+//loose equality means before compareing the values js 
+//convert the data type of values in common type(Type Conversion/Type Coercion)..
 
 //***** strict check*******
 
-console.log("2"=== 2);// false because there datatype are not same
-console.log("2" == 2);// true 
+// console.log("2"=== 2);// false because there datatype are not same
+// console.log("2" == 2);// true 
