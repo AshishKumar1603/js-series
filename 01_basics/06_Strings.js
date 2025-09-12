84c423882fd6d4df9c4d7b77e5c5225c48d68a50
Ashish Kumar <akgautam1603@gmail.com>
2025-09-12 19:57:27 +0000

    string_interpolation

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,7 @@
+ const name = "Hitesh"
+ const repoCount = 50
+
+ // console.log(name + repoCount + "Value") this is old way of using so we didn't prefer this..
+
+ console.log(`Hello my name is ${name} and my repo count is ${repoCount} `)//Tactik use krne se yha p aata hai String interpolation so we make a placeholder
+                //to insert some values in it..
